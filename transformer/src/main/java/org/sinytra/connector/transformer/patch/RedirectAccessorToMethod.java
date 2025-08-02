a799513a321adaba14d07111ffd428c56f6c27a3
cyf680 <chenyufanalex22@163.com>
2025-08-02 08:56:41 +0800

    Update RedirectAccessorToMethod.java

--- transformer/src/main/java/org/sinytra/connector/transformer/patch/RedirectAccessorToMethod.java
+++ transformer/src/main/java/org/sinytra/connector/transformer/patch/RedirectAccessorToMethod.java
@@ -18,7 +18,7 @@ import static org.sinytra.adapter.patch.PatchInstance.MIXINPATCH;
 public record RedirectAccessorToMethod(String value, TransformerEnvironment environment) implements MethodTransform {
     private static final Logger LOGGER = LogUtils.getLogger();
     private static final Marker COMPONENT_MARKER = MarkerFactory.getMarker("COMPONENT");
-    
+
     @Override
     public Collection<String> getAcceptedAnnotations() {
         return Set.of(MixinConstants.ACCESSOR);
@@ -26,37 +26,43 @@ public record RedirectAccessorToMethod(String value, TransformerEnvironment envi
 
     @Override
     public Patch.Result apply(ClassNode classNode, MethodNode methodNode, MethodContext methodContext, PatchContext context) {
-        // 组件系统特殊处理：如果重定向到 getItem() 方法，则使用组件API
+        // Special handling for "getItem" accessors in ItemStack
         String targetMethod = this.value;
         if (isItemAccessor(methodNode, classNode.name)) {
             targetMethod = "getComponentItem";
-            LOGGER.debug(COMPONENT_MARKER, "Redirecting ItemStack accessor to component API: {}.{} -> {}", 
-                        classNode.name, methodNode.name, targetMethod);
+            LOGGER.debug(COMPONENT_MARKER, "Redirecting ItemStack accessor to component API: {}.{} -> {}",
+                    classNode.name, methodNode.name, targetMethod);
         }
-        
+
         AnnotationVisitor visitor = methodNode.visitAnnotation(MixinConstants.INVOKER, true);
         visitor.visit("value", targetMethod);
         visitor.visitEnd();
 
-        methodNode.visibleAnnotations.remove(methodContext.methodAnnotation().unwrap());
+        if (methodNode.visibleAnnotations != null && methodContext.methodAnnotation() != null) {
+            methodNode.visibleAnnotations.remove(methodContext.methodAnnotation().unwrap());
+        }
 
-        LOGGER.info(MIXINPATCH, "Redirecting accessor {}.{} to invoke method {}", 
-                   classNode.name, methodNode.name, targetMethod);
+        LOGGER.info(MIXINPATCH, "Redirecting accessor {}.{} to invoke method {}",
+                classNode.name, methodNode.name, targetMethod);
 
         return Patch.Result.APPLY;
     }
-    
+
     private boolean isItemAccessor(MethodNode method, String className) {
-        // 识别 ItemStack 相关的访问器
-        return className.equals("net/minecraft/world/item/ItemStack") && 
-               method.name.equals("getItem") && 
-               method.desc.equals("()Lnet/minecraft/world/item/Item;");
+        // Recognize ItemStack's getItem accessor signature
+        return "net/minecraft/world/item/ItemStack".equals(className)
+                && "getItem".equals(method.name)
+                && "()Lnet/minecraft/world/item/Item;".equals(method.desc);
     }
-    
-    // 在 ConnectorMod 中需要添加的辅助方法
+
+    // Helper method for ItemStack component access.
+    // NOTE: The following code will only compile if the Minecraft classes are available on the classpath.
+    // If you are building outside a mod dev environment, comment out or guard this block.
+    /*
     public static class ComponentAccessors {
         public static net.minecraft.world.item.Item getComponentItem(net.minecraft.world.item.ItemStack stack) {
             return stack.get(net.minecraft.core.component.DataComponents.ITEM);
         }
     }
+    */
 }
