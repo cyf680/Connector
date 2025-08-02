954832ce309b50d158827f1a0efa1687b28d22e4
cyf680 <chenyufanalex22@163.com>
2025-08-02 11:11:23 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -169,6 +169,7 @@ val depsJar: ShadowJar by tasks.creating(ShadowJar::class) {
     exclude("LICENSE.txt")
 
     dependencies {
+        exclude(dependency("org.ow2.asm:"))
         exclude(dependency("net.sf.jopt-simple:"))
         exclude(dependency("com.google.guava:guava"))
         exclude(dependency("com.google.code.gson:gson"))
